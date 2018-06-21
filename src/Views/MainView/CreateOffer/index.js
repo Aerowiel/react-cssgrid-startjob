ee2e5266da7791076523a3d759c5c916888253eb
Victor moreau <victor.moreau@sogeti.com>
2018-06-21 21:35:09 +0200

    Add interest / dashboard view + css grid

---
+++ src/Views/MainView/CreateOffer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateOffer extends Component {
+  render() {
+    return (
+      <div>
+        Bienvenue sur la page de contact !
+      </div>
+    );
+  }
+}
+
+export default CreateOffer;
