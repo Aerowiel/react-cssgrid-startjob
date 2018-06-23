0012458724b1d14a00c59cc161b4ecebce206c62
Victor moreau <victor.moreau@sogeti.com>
2018-06-23 13:03:16 +0200

    Adding CreateOffer page

--- src/Views/MainView/CreateOffer/index.js
+++ src/Views/MainView/CreateOffer/index.js
@@ -3,8 +3,45 @@ import React, { Component } from 'react';
 class CreateOffer extends Component {
   render() {
     return (
-      <div>
-        Bienvenue sur la page de contact !
+      <div className="wrapperCreateOffer">
+        <div className="containerCreateOffer">
+          <form>
+            <label> Nom de l'entreprise disabled:
+                 <a className="enterpriseOffer disabled"> Sogeti</a>
+            </label>  
+            <select className="secteurOffer">
+                <option>Dev Web</option>
+                <option>Dev Mobile</option>
+                <option>Gaming</option>
+                <option>Arts</option>
+            </select> 
+            <label> Nom de l'offre:
+                <input className="nameOffer" placeholder=""/>
+                <a className="errorMessage errorName"> erreur detectée</a>
+            </label>
+
+            <label> Description : 
+              <input className="descriptionOffer"/>
+              <a className="errorMessage descriptionError">erreur detectée</a>
+            </label>
+            
+            <label> Date : 
+              <input type="date" className="dateOffer"></input>
+              <a className="errorMessage dateError">erreur detectée</a>
+            </label>
+            
+            <table className="requiredSkillsOffer">
+              <tbody>
+                <tr>
+                  <td>Firstname</td>
+                  <td>Lastname</td> 
+                  <td>Age</td>
+                </tr>
+                </tbody>
+            </table>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
