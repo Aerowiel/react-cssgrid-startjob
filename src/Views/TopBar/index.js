ffb82450befa3306a15761cab4472b7db59159a4
Z <florian.pendaries@ynov.com>
2018-07-03 10:54:00 +0200

    sessions

--- src/Views/TopBar/index.js
+++ src/Views/TopBar/index.js
@@ -10,13 +10,21 @@ import {userLogStatus} from '../../App';
 class TopBar extends Component {
   constructor(){
     super();
+    this.state = {
+      login: false
+    }
+
   }
   disconnectUser(){
+    console.log("merguez")
     userLogStatus.isLogOut();
+    this.setState({
+      login : true
+    })
   }
   render() {
     return (
-    <div className="topBar"> 
+    <div className="topBar">
         <div className="wrapperTopBar">
             <a className="title">
               StartJob
@@ -25,7 +33,7 @@ class TopBar extends Component {
             <div className="topListButton">
                 <div className="wrapperTopListButton">
                   <Link to="/userProfile"><button><img src="/src/images/user.png"/></button></Link>
-                  <button onClick={this.disconnectUser.bind(this)}><img src="/src/images/logout.png"/></button>                    
+                  <button onClick={this.disconnectUser.bind(this)}><img src="/src/images/logout.png"/></button>
                 </div>
             </div>
         </div>
